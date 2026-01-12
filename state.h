9373d6fc135a8e1db6bb64504a2cb39ba67d1aa6
Jonathan Kimmitt <jonathan@kimmitt.uk>
2026-01-12 15:09:19 +0000

    Checkin before Clauder review

---
+++ state.h
@@ -0,0 +1,16 @@
+{
+    S_FETCH      ,
+    S_DECIDE_IMM,
+    S_FETCH_IMM ,
+    S_EXEC      ,
+
+    // heap write micro-ops
+    S_HEAP_ALLOC_HDR,
+    S_HEAP_ALLOC_FIELDS,
+    S_CLOSURE_ALLOC_HDR,
+    S_CLOSURE_WRITE_CODE,
+    S_CLOSURE_WRITE_ENV,
+    S_CLOSURE_DONE,			   
+    // trap / ccall
+    S_TRAP_WAIT
+  } state_t;
